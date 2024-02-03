41e84e17d407e4f1bb71e36867bc84d7d39f56c3
Fabrício Silva <fabricio85silva@hotmail.com>
2024-02-03 13:20:17 -0300

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './index.css'
 
+
+//Componentes
+import Nav from './componentes/Nav/Nav'
+
 function App() {
 
   return (
     <>
-     
+     <Nav/>
     </>
   )
 }
---
+++ src/componentes/Nav/Nav.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import styles from "./Nav.module.css";
+
+const Nav = () => {
+  // Controla o container do Menu
+
+  const [menuContainer, setMenuContainer] = useState(null);
+
+  return (
+    <>
+      <nav className={styles.nav_container}>
+        <i
+          class="fa-solid fa-bars"
+          onClick={() => setMenuContainer(1)}
+        ></i>
+
+        <div className={styles.brand}>
+          <h2>
+            NOSSO<span>SOL</span>
+          </h2>
+          <p>painéis solares</p>
+        </div>
+      </nav>
+
+      {menuContainer && (
+        <>
+          <div
+            className={styles.overlay_menu}
+            onClick={() => setMenuContainer(null)}
+          ></div>
+          <div className={styles.menu}>
+          
+            <h3> <i class="fa-solid fa-house"></i> INICIO</h3>
+            <h3> <i class="fa-solid fa-phone"></i> CONTATO</h3>
+            <h3> <i class="fa-solid fa-chart-simple"></i> SIMULAR</h3>
+            <h3> <i class="fa-solid fa-chart-simple"></i>  INFORMAÇÕES</h3>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Nav;
