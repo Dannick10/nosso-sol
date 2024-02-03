6b3bf62bc6dbcf511c2bf8e0258baa58e2fe9895
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-03 15:10:09 -0300

    adicionando header

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './estilos/header/header.css'
 //imgs
 import header_painel from './img/header_painel.svg'
 import header_world from './img/header_world.svg'
+import Reactangle from './img/Rectangle.svg'
 
 //Componentes
 import Nav from './componentes/Nav/Nav'
@@ -16,10 +17,12 @@ function App() {
      <Nav/>
 
       <header>
-        <div className="header_title">NÃO PAGUE MAIS UM ABSURDO NA SUA CONTA DE ENERGIA.</div>
-        <div className="header_svg">
-          <img src={header_painel} alt="imagem de um painel soalr" />
-          <img src={header_world} alt="imagem de energia limpa" />
+        <div className='header_container'>  
+          <div className="header_title">NÃO PAGUE MAIS UM ABSURDO NA SUA CONTA DE <span>ENERGIA</span>.</div>
+          <div className="header_svg">
+            <div  className='svg_world'><img src={header_world} alt="imagem de energia limpa" /></div>
+            <div className='svg_painel'><img src={header_painel} alt="imagem de um painel soalr" /></div>
+          </div>
         </div>
       </header>
      
