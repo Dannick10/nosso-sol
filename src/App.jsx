8a78072a999063dac9b0fc57df4e9f3157cb03f3
Fabrício Silva <fabricio85silva@hotmail.com>
2024-02-12 10:28:52 -0300

    melhorias na responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -47,203 +47,186 @@ function App() {
     <>
       <Nav />
 
-      <header id="header">
-        <div className="header_container">
-          <div className="header_title">
-            NÃO PAGUE MAIS UM ABSURDO NA SUA CONTA DE <span>ENERGIA</span>.
-          </div>
-          <div className="header_svg">
-            <div className="svg_world">
-              <img
-                src={header_world}
-                alt="imagem de energia limpa"
-              />
+      <div className="containerResponsivo">
+        <header id="header">
+          <div className="header_container">
+            <div className="header_title">
+              NÃO PAGUE MAIS UM ABSURDO NA SUA CONTA DE <span>ENERGIA</span>.
             </div>
-            <div className="svg_painel">
-              <img
-                src={header_painel}
-                alt="imagem de um painel soalr"
-              />
+            <div className="header_svg">
+              <div className="svg_world">
+                <img
+                  src={header_world}
+                  alt="imagem de energia limpa"
+                />
+              </div>
+              <div className="svg_painel">
+                <img
+                  src={header_painel}
+                  alt="imagem de um painel soalr"
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div
-          onClick={() => scrollHeader()}
-          className="arrow-down"
-        >
-          <i class="fa-solid fa-arrow-down"></i>
-        </div>
-      </header>
-
-      <main>
-        <section id="showcase_container">
-          <h3>Enérgia Solar</h3>
-          <div className="subtitle">
-            <p>
-              Nos últimos anos os brasileiros têm investido mais em energia
-              limpa. Além de ser mais barato para o bolso, quem investe em
-              energia solar acaba investindo na sua independência energética.
+          <div
+            onClick={() => scrollHeader()}
+            className="arrow-down"
+          >
+            <i class="fa-solid fa-arrow-down"></i>
+          </div>
+        </header>
+        <main>
+          <section id="showcase_container">
+            <h3>Enérgia Solar</h3>
+            <div className="subtitle">
+              <p>
+                Nos últimos anos os brasileiros têm investido mais em energia
+                limpa. Além de ser mais barato para o bolso, quem investe em
+                energia solar acaba investindo na sua independência energética.
+              </p>
+            </div>
+            <span className="line"></span>
+            <path d="M52,253 C246,364 452,106 582,292" />
+            <div className="img_cards">
+              <div className="card-1"></div>
+              <div className="card-2"></div>
+            </div>
+          </section>
+          <section className="info_enegy">
+            <h2 className="title-1">
+              VOCÊ GERA A SUA PRÓPRIA ENERGIA, PAGA MENOS TARIFAS E SOBRA MAIS
+              DINHEIRO NO SEU BOLSO<span>.</span>
+            </h2>
+            <p className="description-1">
+              Temos alguns depoimentos muito legais que mostram que esse é o
+              futuro para quem gosta de ter grana no bolso e de quebra ainda ajuda
+              o meio ambiente.
             </p>
-          </div>
-
-          <span className="line"></span>
-          <path d="M52,253 C246,364 452,106 582,292" />
-
-          <div className="img_cards">
-            <div className="card-1"></div>
-
-            <div className="card-2"></div>
-          </div>
-        </section>
-
-        <section className="info_enegy">
-          <h2 className="title-1">
-            VOCÊ GERA A SUA PRÓPRIA ENERGIA, PAGA MENOS TARIFAS E SOBRA MAIS
-            DINHEIRO NO SEU BOLSO<span>.</span>
-          </h2>
-
-          <p className="description-1">
-            Temos alguns depoimentos muito legais que mostram que esse é o
-            futuro para quem gosta de ter grana no bolso e de quebra ainda ajuda
-            o meio ambiente.
-          </p>
-        </section>
-
-        <Swiper
-        className="swiper"
-          slidesPerView={1}
-          pagination={{ clickable: true }}
-          navigation
-        >
-          <SwiperSlide>
-            <Card
-              img="https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVzc29hfGVufDB8fDB8fHwy"
-              nome="Júlia Ramos"
-              info="Para quem está considerando adotar a energia solar, eu não poderia
-              recomendar o suficiente. É uma decisão que não apenas beneficia o
-              meio ambiente, mas também o seu bolso a longo prazo. Estou muito
-              feliz por ter feito essa escolha e mal posso esperar para ver os
-              benefícios contínuos nos próximos anos."
-            />
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <Card
-              img={Felipe}
-              nome="Felipe Oliveira"
-              info="Estou extremamente satisfeito por ter feito essa decisão e estou
-        ansioso para colher os frutos nos próximos anos. A mudança para a
-        energia solar não só alinhou minha casa com práticas mais
-        sustentáveis, mas também resultou em economias significativas em
-        minhas despesas com energia."
-            />
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <Card
-              img={Luciana}
-              nome="Luciana Santos"
-              info="Estou incrivelmente feliz por ter tomado esse caminho e estou ansiosa para experimentar os benefícios contínuos nos próximos anos. A transição para a energia solar foi uma escolha inteligente e recompensadora em todos os aspectos."
-            />
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <Card
-              img={Sant}
-              nome="Santiago S."
-              info="Desde que instalei os painéis solares em minha casa, minha vida mudou para melhor em tantos aspectos. Além de estar fazendo minha parte para o meio ambiente, estou economizando uma quantia significativa de dinheiro a cada mês."
-            />
-          </SwiperSlide>
-        </Swiper>
-
-        <span
-          id="simular"
-          className="line"
-        ></span>
-
-        <section className="simular-container">
-          <h1>FUJA DAS GRANDES TAXAS E POSSIVÉIS QUEDAS DE ENERGIA.</h1>
-          <p>
-            Se você têm o seu próprio negócio ou deseja adquirir painéis para
-            sua casa, simule agora um orçamento:
-          </p>
-          <button className="btn-simular">
-            SIMULAR <i class="fa-solid fa-arrow-right"></i>
-          </button>
-        </section>
-
-        <aside>
-         <Swiper
-            modules={[EffectFade]}
-            effect="fade"
-            centeredSlides={true}
-            autoplay={{
-              delay: 2000,
-              disableOnInteraction: false,
-            }}
+          </section>
+          <Swiper
+          className="swiper"
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+            navigation
           >
             <SwiperSlide>
-              <div
-                id="contact"
-                className="aside_carousel"
-              >
-                <img
-                  src={Obraum}
-                  alt="Imagem de obras"
-                />
-              </div>
+              <Card
+                img="https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVzc29hfGVufDB8fDB8fHwy"
+                nome="Júlia Ramos"
+                info="Para quem está considerando adotar a energia solar, eu não poderia
+                recomendar o suficiente. É uma decisão que não apenas beneficia o
+                meio ambiente, mas também o seu bolso a longo prazo. Estou muito
+                feliz por ter feito essa escolha e mal posso esperar para ver os
+                benefícios contínuos nos próximos anos."
+              />
             </SwiperSlide>
-
             <SwiperSlide>
-              <div className="aside_carousel">
-                <img
-                  src={Obradois}
-                  alt="Imagem de obras"
-                />
-              </div>
+              <Card
+                img={Felipe}
+                nome="Felipe Oliveira"
+                info="Estou extremamente satisfeito por ter feito essa decisão e estou
+          ansioso para colher os frutos nos próximos anos. A mudança para a
+          energia solar não só alinhou minha casa com práticas mais
+          sustentáveis, mas também resultou em economias significativas em
+          minhas despesas com energia."
+              />
             </SwiperSlide>
-
             <SwiperSlide>
-              <div className="aside_carousel">
-                <img
-                  src={Obratres}
-                  alt="Imagem de obras"
-                />
-              </div>
+              <Card
+                img={Luciana}
+                nome="Luciana Santos"
+                info="Estou incrivelmente feliz por ter tomado esse caminho e estou ansiosa para experimentar os benefícios contínuos nos próximos anos. A transição para a energia solar foi uma escolha inteligente e recompensadora em todos os aspectos."
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Card
+                img={Sant}
+                nome="Santiago S."
+                info="Desde que instalei os painéis solares em minha casa, minha vida mudou para melhor em tantos aspectos. Além de estar fazendo minha parte para o meio ambiente, estou economizando uma quantia significativa de dinheiro a cada mês."
+              />
             </SwiperSlide>
           </Swiper>
-
-          <div className="contact">
-            <h1>FALE COM UM DE NOSSOS REPRESENTANTES:</h1>
-            <div className="btn-contact">
-              <i class="fa-brands fa-whatsapp"></i>
-
-              <i class="fa-solid fa-envelope"></i>
+          <span
+            id="simular"
+            className="line"
+          ></span>
+          <section className="simular-container">
+            <h1>FUJA DAS GRANDES TAXAS E POSSIVÉIS QUEDAS DE ENERGIA.</h1>
+            <p>
+              Se você têm o seu próprio negócio ou deseja adquirir painéis para
+              sua casa, simule agora um orçamento:
+            </p>
+            <button className="btn-simular">
+              SIMULAR <i class="fa-solid fa-arrow-right"></i>
+            </button>
+          </section>
+          <aside>
+           <Swiper
+              modules={[EffectFade]}
+              effect="fade"
+              centeredSlides={true}
+              autoplay={{
+                delay: 2000,
+                disableOnInteraction: false,
+              }}
+            >
+              <SwiperSlide>
+                <div
+                  id="contact"
+                  className="aside_carousel"
+                >
+                  <img
+                    src={Obraum}
+                    alt="Imagem de obras"
+                  />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="aside_carousel">
+                  <img
+                    src={Obradois}
+                    alt="Imagem de obras"
+                  />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="aside_carousel">
+                  <img
+                    src={Obratres}
+                    alt="Imagem de obras"
+                  />
+                </div>
+              </SwiperSlide>
+            </Swiper>
+            <div className="contact">
+              <h1>FALE COM UM DE NOSSOS REPRESENTANTES:</h1>
+              <div className="btn-contact">
+                <i class="fa-brands fa-whatsapp"></i>
+                <i class="fa-solid fa-envelope"></i>
+              </div>
             </div>
-          </div>
-        </aside>
-
-        {backToTop && (
+          </aside>
+          {backToTop && (
+            <div
+              className="arrow-up"
+              onClick={handleBackToTop}
+            >
+              <i class="fa-solid fa-arrow-up"></i>
+            </div>
+          )}
           <div
-            className="arrow-up"
-            onClick={handleBackToTop}
+            className="whatsapp"
+            onClick={() =>
+              window.open(
+                "https://api.whatsapp.com/send?phone=5511987440654&text=Ol%C3%A1,%20gostaria%20de%20ter%20um%20pain%C3%A9l%20solar%20na%20minha%20casa/minha%20empresa.",
+                "_blank"
+              )
+            }
           >
-            <i class="fa-solid fa-arrow-up"></i>
+            <i class="fa-brands fa-whatsapp"></i>
           </div>
-        )}
-
-        <div
-          className="whatsapp"
-          onClick={() =>
-            window.open(
-              "https://api.whatsapp.com/send?phone=5511987440654&text=Ol%C3%A1,%20gostaria%20de%20ter%20um%20pain%C3%A9l%20solar%20na%20minha%20casa/minha%20empresa.",
-              "_blank"
-            )
-          }
-        >
-          <i class="fa-brands fa-whatsapp"></i>
-        </div>
-      </main>
+        </main>
+      </div>
 
       <Footer />
     </>
